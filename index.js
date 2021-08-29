34bb0d811df1dec68aea459ac8839315597f7678
Veresia Hiralall <hiralallveresia@yahoo.com>
2021-08-29 19:51:21 -0400

    commit tree 3e88c60f93dcacf83d06f48ff488326e380c2f4e

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+const record = [
+    {year: "2018", result: "L"},
+    {year: "2017", result: "W"},
+    {year: "2016", result: "N/A"}
+    //...
+  ]
+  superbowlWin = (record) => {
+      let result = record.find(record => record.result === 'W');
+      return result ? result.year : undefined; 
+  }
